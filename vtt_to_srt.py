47cc2f985810c5c47d029c8ecf26462e015eefd5
Soooobiniya <rorngk22@gmail.com>
2023-07-23 03:28:38 +0900

    chrome devtools use

--- vtt_to_srt.py
+++ vtt_to_srt.py
@@ -1,6 +1,8 @@
 f_in=open("./before_convert.vtt","r", encoding='UTF8')
 f_out=open("./after_convert.srt","w", encoding='UTF8')
+
 a = 1
+
 for line in f_in:
     if line == "WEBVTT\n":
         line = ""
@@ -11,5 +13,6 @@ for line in f_in:
         f_out.write(line)
     else:
         f_out.write(line)
+        
 f_in.close()
 f_out.close()
